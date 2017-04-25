444a98c3cc1c6bb1b2c1b4a8fc214099d6fc5d2b
Jakub Juszczak <netghost03@gmail.com>
2017-04-25 11:45:37 +0200

    ✨ Add github link to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,7 @@
 <template>
   <footer>
-    Build with <a href="https://vuejs.org/">Vue.js</a> and <a href="http://vue-chartjs.org/#/">vue-chartjs</a>
+   <span> Build with <a href="https://vuejs.org/">Vue.js</a> and <a href="http://vue-chartjs.org/#/">vue-chartjs</a></span>
+   <span>For issues or feature requests visit <a href="https://github.com/apertureless/npm-stats">Github</a></span>
   </footer>
 </template>
 
@@ -17,6 +18,9 @@
     color: #fff;
     font-size: rem(16);
     padding: rem(30) rem(40);
+    display: flex;
+    justify-content: space-between;
+    flex-flow: row nowrap;
 
     a {
       color: color(robin-egg-blue);
