3fdcb578b1e57a1d7a5b31a862b6b5d096bf249c
Jakub Juszczak <netghost03@gmail.com>
2017-04-30 11:32:19 +0200

    ✨ Add weekly, monthly charts and request on date change

--- src/utils/downloadFormatter.js
+++ src/utils/downloadFormatter.js
@@ -1,5 +1,3 @@
-import {dateToYear} from './dateFormatter'
-
 export function removeDuplicate (a, b) {
   if (a.indexOf(b) < 0) {
     a.push(b)
@@ -7,17 +5,17 @@ export function removeDuplicate (a, b) {
   return a
 }
 
-export const getDownloadsPerYear = (data) => {
+export const groupData = (data, transformer) => {
   return data.reduce((date, current) => {
-    if (date.indexOf(dateToYear(current.day)) < 0) {
-      date.push(dateToYear(current.day))
+    if (date.indexOf(transformer(current.day)) < 0) {
+      date.push(transformer(current.day))
     }
     return date
   }, [])
     .map((date) => {
       return {
         date: date,
-        downloads: data.filter(el => dateToYear(el.day) === date)
+        downloads: data.filter(el => transformer(el.day) === date)
           .map(el => el.downloads)
           .reduce((total, download) => total + download)
       }
