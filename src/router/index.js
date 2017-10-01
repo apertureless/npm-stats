029a9b579f65b6952e0f5e0a54c01e80c2e0395c
Oliver Taylor <olivertaylor18@hotmail.co.uk>
2017-10-01 20:10:05 +0100

    Added vue-meta

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import StartPage from '@/pages/Start'
+import Meta from 'vue-meta'
 
+Vue.use(Meta)
 Vue.use(Router)
 
 export default new Router({
