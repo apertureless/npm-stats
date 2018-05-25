bf9e6e21c1ca9cdbe156248b1038470c0511b246
Jakub <netghost03@gmail.com>
2018-05-25 14:02:07 +0200

    feat(privacy): Remove GA

--- src/main.js
+++ src/main.js
@@ -3,12 +3,6 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import VueAnalytics from 'vue-analytics'
-
-Vue.use(VueAnalytics, {
-  id: 'UA-92766713-2',
-  router
-})
 
 Vue.config.productionTip = false
 
