d9f2e2786e3c6b34519dfc4719aab686d654bfc6
Jakub <netghost03@gmail.com>
2018-05-25 14:03:19 +0200

    feat(privacy): Remove vue-cookie-law as no longer needed

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,15 @@
     <j-header></j-header>
     <router-view></router-view>
     <j-footer></j-footer>
-    <cookie-law theme="dark-lime"></cookie-law>
   </div>
 </template>
 
 <script>
 import JHeader from '@/components/Header'
 import JFooter from '@/components/Footer'
-import CookieLaw from 'vue-cookie-law'
 
 export default {
-  components: { JHeader, JFooter, CookieLaw },
+  components: { JHeader, JFooter },
   name: 'app',
   metaInfo: {
     meta: [
